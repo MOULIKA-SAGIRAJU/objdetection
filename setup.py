dc78062659c6f1083d219f4faa63d518c29292ec
MOULIKA-SAGIRAJU <nadimpallimoulika@gmail.com>
2024-03-08 21:42:42 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'steelpipesdetection',
+    version= '0.0.0',
+    author= 'MOULIKA',
+    author_email= 'nadimpallimoulika@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
